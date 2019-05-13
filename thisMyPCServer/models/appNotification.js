74e947e83d26c965bfcb0daf76215119de135239
Supun <supunlakmal61@gmail.com>
2019-05-13 22:51:29 +0530

    Update appNotification.js

--- thisMyPCServer/models/appNotification.js
+++ thisMyPCServer/models/appNotification.js
@@ -1,26 +1,43 @@
 let mongoose = require('mongoose');
 var appNotification = mongoose.Schema({
     softWareID: {
-        type: String, required: true
-    }, message: {
-        type: String, required: true
-    }, messageType: {
-        type: String, required: true
-    }, softWareID: {
-        type: String, required: true
-    }, dismissible: {
-        type: Number, required: true, default: 1 //
-    },status: {
-        type: Number, required: true, default: 1 //
-    }, addDate: {
-        type: Date, default: Date.now
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    messageType: {
+        type: String,
+        required: true
+    },
+    softWareID: {
+        type: String,
+        required: true
+    },
+    dismissible: {
+        type: Number,
+        required: true,
+        default: 1 //
+    },
+    status: {
+        type: Number,
+        required: true,
+        default: 1 //
+    },
+    addDate: {
+        type: Date,
+        default: Date.now
     }
 })
 let AppNotification = module.exports = mongoose.model('userAndPC', appNotification);
-
-
-
 // create   notification
 module.exports.pcAndOwner = function (notification, callback) {
-    AppNotification.create({'softWareID': notification.softWareID, 'message': notification.message ,  'messageType':notification.messageType,  'dismissible':notification.dismissible}, callback);
+    AppNotification.create({
+        'softWareID': notification.softWareID,
+        'message': notification.message,
+        'messageType': notification.messageType,
+        'dismissible': notification.dismissible
+    }, callback);
 }
