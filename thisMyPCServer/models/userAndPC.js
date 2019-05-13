401115d1aa087272abb2f4c82af1a15771a23e0b
Supun <supunlakmal61@gmail.com>
2019-05-13 22:51:53 +0530

    Update userAndPC.js

--- thisMyPCServer/models/userAndPC.js
+++ thisMyPCServer/models/userAndPC.js
@@ -1,27 +1,32 @@
 let mongoose = require('mongoose');
 var userAndPC = mongoose.Schema({
     userID: {
-        type: String, required: true
-    }, pcKeyPublic: {
-        type: String, required: true
-    }, status: {
-        type: Number, required: true, default: 1 //
-    }, released_date: {
-        type: Date, default: Date.now
+        type: String,
+        required: true
+    },
+    pcKeyPublic: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: Number,
+        required: true,
+        default: 1 //
+    },
+    released_date: {
+        type: Date,
+        default: Date.now
     }
 })
 let UserAndPC = module.exports = mongoose.model('userAndPC', userAndPC);
-
-
 // create   User and pc public access
 module.exports.createNewUserAndPC = function (userPC, callback) {
-    UserAndPC.create({'userID': userPC.userID, 'pcKeyPublic': userPC.pcKeyPublic}, callback);
+    UserAndPC.create({
+        'userID': userPC.userID,
+        'pcKeyPublic': userPC.pcKeyPublic
+    }, callback);
 }
-
-
-
 //  get  pc  using pc key
 module.exports.getUserAndPCUsingKey = function (key, callback) {
     UserAndPC.findOne().where("pcKeyPublic", key).exec(callback);
 }
-
