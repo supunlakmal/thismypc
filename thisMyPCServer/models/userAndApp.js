90e2859fe263125625b3edfa48d061d065a119c5
Supun <supunlakmal61@gmail.com>
2019-05-13 22:51:49 +0530

    Update userAndApp.js

--- thisMyPCServer/models/userAndApp.js
+++ thisMyPCServer/models/userAndApp.js
@@ -2,22 +2,36 @@ let mongoose = require('mongoose');
 var ObjectId = mongoose.Types.ObjectId;
 var userAndApp = mongoose.Schema({
     userID: {
-        type: mongoose.Schema.ObjectId, required: true
-    }, appID: {
-        type: mongoose.Schema.ObjectId, required: true
-    }, status: {
-        type: Number, required: true, default: 1 //
-    }, date: {
-        type: Date, default: Date.now
+        type: mongoose.Schema.ObjectId,
+        required: true
+    },
+    appID: {
+        type: mongoose.Schema.ObjectId,
+        required: true
+    },
+    status: {
+        type: Number,
+        required: true,
+        default: 1 //
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
 })
 let UserAndApp = module.exports = mongoose.model('userAndApp', userAndApp);
 // create   User and app
 module.exports.createNewUserAndApp = function (userApp, callback) {
-    UserAndApp.create({'userID': userApp.id, 'appID': userApp.appID}, callback);
-}// delete   User and app
+    UserAndApp.create({
+        'userID': userApp.id,
+        'appID': userApp.appID
+    }, callback);
+} // delete   User and app
 module.exports.deleteUserAndApp = function (userApp, callback) {
-    UserAndApp.remove({'userID': userApp.id, 'appID': userApp.appID}, callback);
+    UserAndApp.remove({
+        'userID': userApp.id,
+        'appID': userApp.appID
+    }, callback);
 }
 // create   User and pc public access
 module.exports.userApps = function (userApp, callback) {
@@ -27,12 +41,17 @@ module.exports.userApps = function (userApp, callback) {
         }
     }, {
         '$lookup': {
-            'from': 'apps', 'localField': 'appID', 'foreignField': '_id', 'as': 'apps'
+            'from': 'apps',
+            'localField': 'appID',
+            'foreignField': '_id',
+            'as': 'apps'
         }
     }], callback);
 }
 //  get  app using userID and  app  ID
 module.exports.getUserAndApp = function (data, callback) {
-    UserAndApp.findOne().where({"userID": data.id, "appID": data.appID}).exec(callback);
+    UserAndApp.findOne().where({
+        "userID": data.id,
+        "appID": data.appID
+    }).exec(callback);
 }
-
