181329affafbcb61dbacd4afb330cf24d0c43c1f
Supun <supunlakmal61@gmail.com>
2019-05-21 21:32:48 +1100

    Update appNotification.js
    ES Lint Format

--- thisMyPCServer/models/appNotification.js
+++ thisMyPCServer/models/appNotification.js
@@ -1,43 +1,43 @@
-let mongoose = require('mongoose');
-var appNotification = mongoose.Schema({
-    softWareID: {
-        type: String,
-        required: true
-    },
-    message: {
-        type: String,
-        required: true
-    },
-    messageType: {
-        type: String,
-        required: true
-    },
-    softWareID: {
-        type: String,
-        required: true
-    },
-    dismissible: {
-        type: Number,
-        required: true,
-        default: 1 //
-    },
-    status: {
-        type: Number,
-        required: true,
-        default: 1 //
-    },
-    addDate: {
-        type: Date,
-        default: Date.now
-    }
-})
-let AppNotification = module.exports = mongoose.model('userAndPC', appNotification);
+const mongoose = require('mongoose');
+const appNotification = mongoose.Schema({
+  softWareID: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  messageType: {
+    type: String,
+    required: true,
+  },
+  softWareID: {
+    type: String,
+    required: true,
+  },
+  dismissible: {
+    type: Number,
+    required: true,
+    default: 1, //
+  },
+  status: {
+    type: Number,
+    required: true,
+    default: 1, //
+  },
+  addDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const AppNotification = module.exports = mongoose.model('userAndPC', appNotification);
 // create   notification
-module.exports.pcAndOwner = function (notification, callback) {
-    AppNotification.create({
-        'softWareID': notification.softWareID,
-        'message': notification.message,
-        'messageType': notification.messageType,
-        'dismissible': notification.dismissible
-    }, callback);
-}
+module.exports.pcAndOwner = function(notification, callback) {
+  AppNotification.create({
+    'softWareID': notification.softWareID,
+    'message': notification.message,
+    'messageType': notification.messageType,
+    'dismissible': notification.dismissible,
+  }, callback);
+};
