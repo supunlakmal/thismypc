3a36e1ac56bb350bf3394aeac8fcff6f3340dbdc
supun156 <supun@smallaxe.tech>
2019-06-12 21:34:03 +0530

    Update App.js

--- thisMyPCMobileApp/App.js
+++ thisMyPCMobileApp/App.js
@@ -13,7 +13,7 @@ const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    'Shake or press menu button for dev menu+',
 });
 
 type Props = {};
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to This My PC</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
