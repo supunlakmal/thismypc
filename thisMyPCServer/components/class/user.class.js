5a942dbeb5d018497583b8b14e2ee668393a1cb2
Supun <supunlakmal61@gmail.com>
2019-07-13 10:25:13 +0530

    User class

---
+++ thisMyPCServer/components/class/user.class.js
@@ -0,0 +1,21 @@
+  class User{
+    constructor(userData){
+        this.setUserObject = userData;
+    this.user = {};
+    }
+userInformation(){
+    this.user.userID = this.setUserObject._id;
+    this.user.firstName = this.setUserObject.name;
+    this.user.lastName = this.setUserObject.nameLast;
+    this.user.email = this.setUserObject.email;
+return this;
+}
+withAuthentication(){
+    this.user.authentication_key = this.setUserObject.auth;
+    return this;
+}
+get(){
+    return  this.user;
+}
+}
+module.exports = User;
