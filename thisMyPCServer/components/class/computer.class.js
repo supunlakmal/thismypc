9eb9599444dfa08b0ac9d4aad6b4568a3eb5b585
Supun <supunlakmal61@gmail.com>
2019-07-14 19:16:29 +0530

    Create computer.class.js

---
+++ thisMyPCServer/components/class/computer.class.js
@@ -0,0 +1,30 @@
+  class Computer{
+    constructor(computerData){
+        this.setComputerObject = computerData;
+    this.computer = {};
+    }
+computerInformation(){
+    this.computer.computerID = this.setComputerObject._id;
+   
+return this;
+}
+withAuthentication(){
+    this.computer.authentication_key = this.setComputerObject.authApp;
+    return this;
+}
+
+
+withUserInformation(userObject){
+    this.computer.userID = userObject._id;
+    this.computer.firstName = userObject.name;
+    this.computer.lastName = userObject.nameLast;
+    this.computer.email = userObject.email;
+    return this;
+}
+
+
+get(){
+    return  this.computer;
+}
+}
+module.exports = Computer;
