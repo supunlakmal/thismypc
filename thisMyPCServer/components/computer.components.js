74531a5ff52d2269bb715648d2be4f743df6a660
Supun <supunlakmal61@gmail.com>
2019-07-24 16:21:13 +0530

    Update computer.components.js

--- thisMyPCServer/components/computer.components.js
+++ thisMyPCServer/components/computer.components.js
@@ -1,4 +1,3 @@
-
 /**
  * User Module
  */
@@ -10,16 +9,51 @@ const UserComponent = require('./user.components');
 class ComputerComponent extends UserComponent {
   constructor() {
     super();
-  
   }
-  
-
-  async authentication(res, userID, authentication_key ,computerKey) {
+  /**
+   * Get computer  Key
+   */
+  getComputerKey() {}
+  /**
+   * User ID
+   */
+  getUserID() {}
+  /**
+   * Computer  public access key that  alow to other user can use computer
+   */
+  getPublicAccessKey() {}
+  /**
+   * Public access status
+   */
+  getPublicAccessStatus() {}
+  /**
+   * Computer Name
+   */
+  getPcName() {}
+  /**
+   * Compter Platform
+   */
+  getPlatform() {}
+  /**
+   * Is  computer online
+   */
+  getPcOnline() {}
+  /**
+   * get computer authentication
+   */
+  getComputerAuthentication() {}
+  /**
+   *
+   * @param {Object} res
+   * @param {String} userID
+   * @param {String} authentication_key
+   * @param {String} computerKey
+   */
+  async authentication(res, userID, authentication_key, computerKey) {
     if (!await PC.authApp(userID, authentication_key, computerKey)) {
       res.status(401);
       return res.json(this.respond(false, 'Invalid User', null));
     }
   }
-
 }
 module.exports = ComputerComponent;
