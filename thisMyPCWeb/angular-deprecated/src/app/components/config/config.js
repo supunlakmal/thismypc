3e66710f04ec67c3d0975cafd413a004b6a78d5f
Supun Lakmal Abesekara <supunlakmal61@gmail.com>
2025-04-03 03:48:28 +0000

    Remove deprecated files and assets from the project

--- thisMyPCWeb/angular-deprecated/src/app/components/config/config.js
+++
@@ -1,7 +0,0 @@
-let config = {
-  "url": "http://localhost",
-  "port": ":5000",
-}
-export {
-  config
-}; // a list of exported variable
--- thisMyPCWeb/react_js/src/redux/root-reducer.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import auth from "./auth/authReducer";
-
-export default combineReducers({
-  auth,
-});
